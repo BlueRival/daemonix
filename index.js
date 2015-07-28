b20faeeb21a600a8abca7654d0798be618e322be
Anthony Hildoer <anthony@bluerival.com>
2015-07-27 23:48:16 -0600

    release 1.0.0

--- index.js
+++ index.js
@@ -1,47 +1,32 @@
-"use strict";
+'use strict';
 
-(function () {
+(function() {
 
   var Daemonix = require( './lib/daemonix' );
-  var Container = require( 'sidi' ).Container;
-  var container = new Container();
+
   var daemonix = null;
 
-  container.set( 'cluster', require( 'cluster' ) );
-  container.set( 'os', require( 'os' ) );
-  container.set( 'process', process );
-
-  module.exports = function ( config ) {
+  module.exports = function( container ) {
 
     // only generate one of these
     if ( daemonix === null ) {
 
-      // allow overrides
-      if ( config instanceof Container ) {
-
-        var fields = [
-          'config',
-          'cluster',
-          'os',
-          'process',
-          'scribe'
-        ];
-        var field = null;
-
-        for ( var i = 0; i < fields.length; i++ ) {
-          field = config.get( fields[i] );
-          if ( typeof field !== 'undefined' ) {
-            container.set( fields[i], field );
-          }
+      var defaults = {
+        cluster: require( 'cluster' ),
+        os:      require( 'os' ),
+        process: process,
+        scribe:  function() {
+          // NO-OP
         }
+      };
 
-      } else {
-
-        container.set( 'config', config );
-
-        daemonix = new Daemonix( container );
-
+      for ( var field in defaults ) {
+        if ( defaults.hasOwnProperty( field ) && !container.has( field ) ) {
+          container.set( field, defaults[ field ] );
+        }
       }
+
+      daemonix = new Daemonix( container );
 
     }
 
